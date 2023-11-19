996325b6a8e313b018f8cd17b9f342ee1eb74af5
Daniel Penaloza <danielpenalozatj@gmail.com>
2023-11-19 09:23:27 -0800

    creation of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+require 'faker'
+
+100.times do |i|
+  rand_bool = [true, false].sample
+
+  Expense.create!(payee: Faker::Commerce.product_name, 
+                  amount: Faker::Number.decimal(l_digits: 3),
+                  expense_date: Faker::Date.between(from: '2020-01-01', to: '2024-01-01'),
+                  active: rand_bool)
+end

1b5cc6604cb03a05f5ed3c07bdb6c2d61c54b89f
Daniel Penaloza <danielpenalozatj@gmail.com>
2023-12-05 19:19:39 -0800

    installing swagger

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     namespace :v1 do
       resources :expenses, only: %i[index create update show destroy]
